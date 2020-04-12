fda85c56d0f433021c534455227266f03fac80ab
egconley <31393715+egconley@users.noreply.github.com>
2020-04-12 15:57:21 -0700

    fixed links to app.js and style.css, deleted TableController

--- src/main/java/com/egconley/Auth0TechnicalExercise/controllers/HomeController.java
+++ src/main/java/com/egconley/Auth0TechnicalExercise/controllers/HomeController.java
@@ -1,23 +1,47 @@
 package com.egconley.Auth0TechnicalExercise.controllers;
 
 import com.auth0.SessionUtils;
+import com.egconley.Auth0TechnicalExercise.models.AccountRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("unused")
+@Component
+@Configuration
+@PropertySource("classpath:auth0.properties")
 @Controller
 public class HomeController {
 
+    // TODO: (optional) add additional tenants
+    @Value(value = "${com.auth0.tenant}")
+    private String tenantName;
+
+    @Value(value = "${com.auth0.APIClientID}")
+    private String apiclientId;
+
+    /**
+     * This is the client secret for the Management API/v2
+     */
+    @Value(value = "${com.auth0.APIClientSecret}")
+    private String apiclientSecret;
+
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @RequestMapping(value = "/portal/home", method = RequestMethod.GET)
-    protected String home(final Map<String, Object> model, final HttpServletRequest req) {
+    @GetMapping(value = "/portal/home")
+    protected String home(final Map<String, Object> model, final HttpServletRequest req, Model m) {
         logger.info("Home page");
         String accessToken = (String) SessionUtils.get(req, "accessToken");
         String idToken = (String) SessionUtils.get(req, "idToken");
@@ -26,7 +50,15 @@ public class HomeController {
         } else if (idToken != null) {
             model.put("userId", idToken);
         }
-        return "home";
+
+        List<String> tenantNames = new LinkedList<>();
+        // TODO: (optional) add additional tenants
+        tenantNames.add(tenantName);
+        AccountRules rules = new AccountRules();
+        HashMap<String, List<String>> rulesByApp = rules.getAllClientRules(tenantNames, apiclientId, apiclientSecret);
+        m.addAttribute("data", rulesByApp);
+
+        return "table";
     }
 
 }
